b4155aee7c9274c9aceb2333b6aed4ef2216dbd4
Rodrigo <rss.999999999@gmail.com>
2023-09-01 16:13:34 -0300

    Initial commit

---
+++ Estrutura Sequencial/src/Concatenar.java
@@ -0,0 +1,30 @@
+/**
+ * Estrutura Sequencial.
+ *
+ * Autor: Rodrigo Soares Sampaio
+ * Data: 01/09/2023
+ * Versão: 0.1
+ *
+ * "%s": Especificador para substituir por uma string. É usado quando você deseja inserir um valor de tipo String na string formatada.
+ * "%d": Especificador para substituir por um número inteiro (tipo int). É usado quando você deseja inserir um valor inteiro na string formatada.
+ * "%f": Especificador para substituir por um número de ponto flutuante (tipo float ou double). É usado quando você deseja inserir um valor de ponto flutuante na string formatada.
+ *
+ *
+ */
+import java.util.Locale;
+
+public class Concatenar {
+    public static void main(String[] args) {
+        // Definindo o Locale padrão como US para formatação de números
+        Locale.setDefault(Locale.US);
+
+        // Declarando e inicializando variáveis
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+
+        // Imprimindo a frase formatada com os valores das variáveis
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+    }
+}
+
---
+++ Estrutura Sequencial/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US); //Para considerar o separador de decimais como ponto
+        int x = 10;
+
+        System.out.print("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println(x);
+        System.out.printf("%.2f%n", (double) x); // Corrected format specifier and argument
+        System.out.printf("%.4f%n", (double) x); // Corrected format specifier and argument
+        System.out.println("Resultado = " + x + " metros"); //concatenar elementos
+        System.out.printf("Resultado %.2f metros%n", (double) x); //concatenar elementos, casting. n=quebbrarLinha; f=ponto;
+
+
+
+
+    }
+}
