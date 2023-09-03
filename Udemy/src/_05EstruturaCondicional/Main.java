4d4d480b4cbe9ca9e98272368de85fcce76c2f55
Rodrigo <rss.999999999@gmail.com>
2023-09-03 15:22:19 -0300

    Ler dados

--- Udemy/src/_05EstruturaCondicional/Main.java
+++ Udemy/src/_05EstruturaCondicional/Main.java
@@ -1,7 +1,26 @@
 package _05EstruturaCondicional;
-//import java.util.Locale;
+import java.util.Locale;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double minutos;
+        double valor = 0;
+
+        System.out.print("Digite a quantidade de minutos: ");
+        minutos = sc.nextDouble();
+
+        if (minutos > 100.0) {
+            valor = (minutos - 100.0) * 2.0 + 50.0;
+        } else {
+            valor = 50.0;
+        }
+        System.out.printf("Valor a pagar: R$ %.2f ", valor);
+        sc.close();
     }
 }
