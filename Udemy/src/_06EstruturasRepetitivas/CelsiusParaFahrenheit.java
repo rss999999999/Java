d3c2115f73fa4fa61047d963b985e693df437892
Rodrigo <rss.999999999@gmail.com>
2023-09-06 06:19:52 -0300

    Celsius for Fahrenheit using do{while}}

---
+++ Udemy/src/_06EstruturasRepetitivas/CelsiusParaFahrenheit.java
@@ -0,0 +1,32 @@
+package _06EstruturasRepetitivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+Fazer um programa para ler uma temperatura em Celsius e mostrar o equivalente em
+Fahrenheit. Perguntar se o usuário deseja repetir (s/n). Caso o usuário digite "s", repetir o
+programa.
+Fórmula: F = 9.0 * C / 5.0 + 32.0
+ */
+public class CelsiusParaFahrenheit{
+
+        public static void main(String[] args) {
+
+            Locale.setDefault(Locale.US);
+            Scanner sc = new Scanner(System.in);
+
+            char resp;
+
+            do {
+                System.out.println("Digite a temperatura em Celsius: ");
+                double c = sc.nextDouble();
+                double f = 9.0 * c / 5.0 + 32.0;
+                System.out.printf("Equivalente em Fahrenheit: %.1f%n", f);
+                System.out.println("Deseja repetir (s/n)? ");
+                resp = sc.next().charAt(0);
+            } while (resp != 'n');
+
+            sc.close();
+        }
+}
