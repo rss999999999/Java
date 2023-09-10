8c9408745084665c24cb2919d98f0395da475298
Rodrigo <rss.999999999@gmail.com>
2023-09-10 08:24:49 -0300

    Exercícios (while)

---
+++ Udemy/src/_06EstruturasRepetitivas/ExerciciosPropostos.java
@@ -0,0 +1,25 @@
+package _06EstruturasRepetitivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExerciciosPropostos {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int password = 0;
+
+        while (password != 2002) {
+            System.out.println("Digite a senha:");
+            password = sc.nextInt();
+            if (password != 2002) {
+                System.out.println("Senha Invalida");
+            }
+        }
+        System.out.println("Acesso Concedido");
+
+        sc.close();
+    }
+}
