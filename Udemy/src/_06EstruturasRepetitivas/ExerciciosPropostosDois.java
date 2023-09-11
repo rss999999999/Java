c0e447f18d25700ec81cf36104171f3a50dbc1e6
Rodrigo <rss.999999999@gmail.com>
2023-09-11 07:36:42 -0300

    Exercícios (do-while): ExerciciosPropostosDois.java

--- Udemy/src/_06EstruturasRepetitivas/ExerciciosPropostosDois.java
+++ Udemy/src/_06EstruturasRepetitivas/ExerciciosPropostosDois.java
@@ -1,3 +1,9 @@
+/*
+Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma)
+*/
+
 package _06EstruturasRepetitivas;
 
 import java.util.Scanner;
