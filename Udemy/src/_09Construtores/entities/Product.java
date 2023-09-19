e939228b388a9c5a3e036040f85620267c4b52e2
Rodrigo <rss.999999999@gmail.com>
2023-09-19 16:52:53 -0300

    Construtores (aula 75)

--- Udemy/src/_09Construtores/entities/Product.java
+++ Udemy/src/_09Construtores/entities/Product.java
@@ -1,9 +1,19 @@
 package _09Construtores.entities;
 
 public class Product {
+    //atributos da classe
     public String name;
     public double price;
     public int quantity;
+
+    //constructor
+    //existe a sobrecarga que é a possibilidade de ter mais de um construtor na classe
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
 
     public double totalValueInStock() {
         return price * quantity;
